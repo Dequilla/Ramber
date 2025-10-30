daee66c9e520c5823fbd6c73ad3c39952a2c1877
Edwin Wallin <edwin@autopol.com>
2025-10-30 09:44:29 +0100

    Fixed .inl file for Mat4 to work with clangd. Added missing include.

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -1,14 +1,17 @@
 #define RAMBER_IMPORT
 #include <ramber/ramber.hpp>
 
-int main( int argc, char **argv )
-{
-    ramber::hello_world();
+#include <format>
 
-    ramber::Vec3f vec{ 10.f, 10.f, 10.f };
-    std::cout << std::string( vec ) << std::endl;
+int main(int argc, char **argv) {
+  ramber::hello_world();
 
-    ramber::Mat4<float> mat = ramber::Mat4<float>::identity();
-    std::cout << std::string( mat ) << std::endl;
-    return 0;
+  ramber::Vec3f vec{10.f, 10.f, 10.f};
+  std::cout << std::string(vec) << std::endl;
+
+  ramber::Mat4<float> mat = ramber::Mat4<float>::identity();
+  std::cout << std::string(mat) << std::endl;
+
+  std::cout << std::format("\"{}\"", std::string(mat)) << std::endl;
+  return 0;
 }
